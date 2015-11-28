928ccdc3869b2aa13f6f0466b6dad9f639365c33
Isaiah Ballah <isaiahballah@gmail.com>
2015-11-28 08:10:36 -0500

    work in progress
    added .gitignore for node_modules

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+	"secret" : "wowsonwheredyoufindthis",
+	"database": "mongodb://isaiah:pass@ds055564.mongolab.com:55564/testing"
+}
---
+++ config/database.js
@@ -0,0 +1,4 @@
+// config/database.js
+module.exports = {
+    'url' : "mongodb://isaiah:pass@ds055564.mongolab.com:55564/testing" // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+};
